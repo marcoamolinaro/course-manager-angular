4a9e9a02692a288792d3da1a9a9bb35db32fa8f0
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2021-03-26 09:57:57 -0300

    Criando form course-info

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { CourseService } from "./course.service";
+import { Course } from './course'
 
 @Component({
     templateUrl: './course-info.component.html',
 })
 export class CourseInfoComponent implements OnInit {
 
-    courseId: number;
+    course: Course;
 
-    constructor(private activateRoute: ActivatedRoute) {}
+    constructor(private activateRoute: ActivatedRoute, private courseServive: CourseService) {}
 
     ngOnInit(): void {
-        this.courseId = +this.activateRoute.snapshot.paramMap.get('id');
+        this.course = 
+            this.courseServive.retrieveById(
+                +this.activateRoute.snapshot.paramMap.get('id'));
+    }
+
+    save(): void {
+        this.courseServive.save(this.course);
     }
 }
