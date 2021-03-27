9f3b7d6b20e2ae14b9c33775e185b572946d230f
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2021-03-26 22:03:14 -0300

    Criado StarModue e AppPipeModule

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -2,21 +2,21 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { ReplacePipe } from "../pipe/replace.pipe";
-import { StarComponent } from "../star/star.compoment";
+import { StarModule } from "../shared/component/star/star.module";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
+import { AppPipeModule } from "../shared/pipe/app-pipe.module";
 
 @NgModule({
     declarations: [
         CourseInfoComponent,
         CourseListComponent,
-        ReplacePipe,
-        StarComponent
     ],
     imports: [
         FormsModule,
         CommonModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: 'courses/info/:id', component: CourseInfoComponent
---
+++ src/app/shared/component/star/star.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { StarComponent } from "./star.compoment";
+
+@NgModule({
+    declarations:[
+        StarComponent
+    ],
+    exports: [
+        StarComponent
+    ]
+})
+export class StarModule {
+
+}
